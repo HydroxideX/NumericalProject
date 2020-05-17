511cbe27258ea56903332e111a806d7ef1c1ee60
sh3ra <youssefalshaarawi@yahoo.com>
2020-05-17 23:39:36 +0200

    false position

--- src/FalsePosition.py
+++ src/FalsePosition.py
@@ -1,17 +1,17 @@
-# assume f(xl)f(xu)<0
-def bisection(xl, xu, func, eps, n):
+# assume f(xl)<0 f(xu)>0
+def false_position(xl, xu, func, eps, n):
     if n is None:
         n = 50
     if eps is None:
         eps = 0.00001
     current_eps = 65868541546874571685758714357356413541255000000135405
-    xr = (xl + xu) / 2
+    xr = ((xl*func(xu))-(xu*func(xl)))/(func(xu)-func(xl))
     root = []
     for i in range(1, n):
-        if (func(xl) * func(xr)) < 0:
+        if (func(xr)) > 0:
             xr_old = xr
             xu = xr
-        elif func(xr) * func(xl) == 0:
+        elif func(xr) == 0:
             root.append((i, xl, xu, xr, current_eps))
             break
         else:

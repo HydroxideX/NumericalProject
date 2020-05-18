367c58b90b404da439c567968e9a8150b782729c
sh3ra <youssefalshaarawi@yahoo.com>
2020-05-18 16:28:19 +0200

    fixedPoint

---
+++ src/FixedPoint.py
@@ -0,0 +1,18 @@
+def fixed_point(xo, eps, n, g):
+    if n is None:
+        n = 50
+    if eps is None:
+        eps = 0.00001
+    current_eps = 65868541546874571685758714357356413541255000000135405
+    xr = xo
+    root = []
+    for i in range(1, n):
+        xr_old = xr
+        xr = g(xr_old)
+        if i > 1:
+            current_eps = (xr - xr_old) / xr
+            if current_eps < eps:
+                root.append((i, xr, current_eps))
+                break
+        root.append((i, xr, current_eps))
+    return root
